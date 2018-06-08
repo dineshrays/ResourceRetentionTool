04b49d7400b50b8df738ebaa29b384e4c4437609
akhil <akhilgelli1141@gmail.com>
2018-06-08 15:51:27 +0530

    AK:Created new controller and index for training

---
+++ RetentionTool/RetentionTool/Controllers/TrainingController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RetentionTool.Controllers
+{
+    public class TrainingController : Controller
+    {
+        // GET: Training
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
