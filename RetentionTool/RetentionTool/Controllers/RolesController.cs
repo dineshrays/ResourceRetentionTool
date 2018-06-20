fe72f44a88676d68b5f630083f66d32e6e122d9a
Group10 <dinesh.rays@gmail.com>
2018-06-20 10:36:14 +0530

    VI:Modification in Employee Evaluation Task and Roles controller

---
+++ RetentionTool/RetentionTool/Controllers/RolesController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RetentionTool.Models;
+using RetentionTool.ViewModel;
+namespace RetentionTool.Controllers
+{
+    public class RolesController : Controller
+    {
+        RetentionToolEntities db = new RetentionToolEntities();
+        // GET: Roles
+        public ActionResult Index()
+        {
+            List<Role> role = db.Roles.Where(a => a.IsActive == true).ToList();
+            RolesViewModel roleviewmodel = new RolesViewModel();
+            roleviewmodel.rolevm = role;
+            return View(roleviewmodel);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Role role)
+        {
+
+            return View();
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Edit(int id,Role role)
+        {
+
+            return View();
+        }
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Delete(int id, Role role)
+        {
+
+            return View();
+        }
+
+    }
+}
---
+++ RetentionTool/RetentionTool/ViewModel/RolesViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using RetentionTool.Models;
+
+namespace RetentionTool.ViewModel
+{
+    public class RolesViewModel
+    {
+        public List<Role> rolevm = new List<Role>();
+        public Role role;
+    }
+}
