fb6f311e53e4cfdbb321e7f7e32be96f4e723aed
akhil <akhilgelli1141@gmail.com>
2018-07-23 19:42:50 +0530

    AK: modified Trainer

--- RetentionTool/RetentionTool/Controllers/TrainerController.cs
+++ RetentionTool/RetentionTool/Controllers/TrainerController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+
 namespace RetentionTool.Controllers
 {
     public class TrainerController : Controller
@@ -28,6 +29,36 @@ namespace RetentionTool.Controllers
         {
 
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Trainer trainer)
+        {
+            if (ModelState.IsValid)
+            {
+                trainer.IsActive = true;
+                db.Trainers.Add(trainer);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(trainer);
+        }
+
+        public ActionResult getTrainerName(string name)
+        {
+            List<TrainerModel> va = (from perso in db.PersonalInfoes
+                                        join trin in db.Trainers
+                                on perso.Id equals trin.PersonalInfo_Id  
+                                
+                                     
+                                     select new TrainerModel
+                                     {
+                                         Id = perso.Id,
+                                         Name = perso.Name
+                                     }).ToList();
+            return new JsonResult { Data = va, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
         }
     }
 }
