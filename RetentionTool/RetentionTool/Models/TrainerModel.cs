fbe9d0286367173be95ebb67bd0e95ce3e8ceb60
akhil <akhilgelli1141@gmail.com>
2018-07-23 17:48:07 +0530

    AK:Modifications on trainer

---
+++ RetentionTool/RetentionTool/Models/TrainerModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RetentionTool.Models
+{
+    public class TrainerModel
+    {
+        public long Id { get; set; }
+        public String Name { get; set; }
+        public Nullable<int> PersonalInfo_Id { get; set; }
+        public Nullable<bool> IsActive { get; set; }
+        public Nullable<int> CriticalResource_Id { get; set; }
+        
+        public virtual ICollection<AssignEvaluater> AssignEvaluaters { get; set; }
+        public virtual ICollection<AssignResource> AssignResources { get; set; }
+        public virtual CriticalResource CriticalResource { get; set; }
+        public virtual ICollection<EmployeeEvalTask> EmployeeEvalTasks { get; set; }
+        public virtual PersonalInfo PersonalInfo { get; set; }
+    }
+}
