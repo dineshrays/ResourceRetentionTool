63b476c0685d51ff32bd69e95e8e059ddb5fe03e
akhil <akhilgelli1141@gmail.com>
2018-07-31 16:29:49 +0530

    AK:Updating Code

---
+++ RetentionTool/RetentionTool/Areas/Manager/Controllers/EmpSearchController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RetentionTool.Areas.Manager.Controllers
+{
+    public class EmpSearchController : Controller
+    {
+        // GET: Manager/EmpSearch
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
