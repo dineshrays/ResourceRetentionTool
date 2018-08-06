118ecbfaaefa9ed407a4bc2f37ab7425e85b39db
Group10 <dinesh.rays@gmail.com>
2018-08-06 12:30:17 +0530

    VI:Modification in Employee Evaluation

--- RetentionTool/RetentionTool/Areas/Trainer/Controllers/EmployeeEvaTasklController.cs
+++ RetentionTool/RetentionTool/Areas/Trainer/Controllers/EmployeeEvaTasklController.cs
@@ -30,14 +30,16 @@ namespace RetentionTool.Areas.Trainer.Controllers
         //    ViewBag.asseval = assignEval;
         //    List<EmployeeEvalTask> empevaltask = db.EmployeeEvalTasks.Where(a => a.IsActive == true).Distinct().ToList();
         //    ViewBag.empevalDetails = empevaltask;
-            List<AssignResource> assignResources = db.AssignResources.Where(a =>db.Trainers.Any(t => t.Id == a.Trainer_Id && t.PersonalInfo_Id == td && t.IsActive == true) &&
-                                                                           (db.AssignEvaluaters.Any(p2 => p2.AssignResource_Id == a.Id && p2.IsActive == true && a.IsActive == true)
-                                                                           || db.EmployeeEvalTasks.Any(e => e.AssignResource_Id == a.Id && e.IsActive == true && 
-                                                                           db.EmployeeEvalTaskDets.Any(t => t.EmployeeEvalTask_Id == e.Id && t.IsEligiableMark == true && t.IsActive == true)))
+            List<AssignResource> assignResources = db.AssignResources.Where(a =>
+           db.AssignEvaluaters.Any(p2 => p2.AssignResource_Id == a.Id && db.Trainers.Any(t => t.Id == p2.Trainer_Id && t.PersonalInfo_Id == td && t.IsActive == true) && p2.IsActive == true && a.IsActive == true)
+                                                                           || db.EmployeeEvalTasks.Any(e => e.AssignResource_Id == a.Id
+                                                                           && db.Trainers.Any(t => t.Id == e.Trainer_Id && t.PersonalInfo_Id == td && t.IsActive == true)
+                                                                           && e.IsActive == true && 
+                                                                           db.EmployeeEvalTaskDets.Any(t => t.EmployeeEvalTask_Id == e.Id && t.IsEligiableMark == true && t.IsActive == true))
                                                                             ).ToList();
             ViewBag.assResDetails = assignResources;
 
-            List<AssignEvaluater> assignEval = db.AssignEvaluaters.Where(a => a.IsActive == true).ToList();
+            List<AssignEvaluater> assignEval = db.AssignEvaluaters.Where(a => a.IsActive == true &&  db.Trainers.Any(t => t.Id == a.Trainer_Id && t.PersonalInfo_Id == td && t.IsActive == true)).ToList();
             ViewBag.asseval = assignEval;
             //db.Trainers.Any(b=>b.Id==a.Trainer_Id && b.IsActive==true && b.PersonalInfo_Id==td ) &&
             List<EmployeeEvalTask> empevaltask = db.EmployeeEvalTasks.Where(a =>  a.IsActive == true).Distinct().ToList();
