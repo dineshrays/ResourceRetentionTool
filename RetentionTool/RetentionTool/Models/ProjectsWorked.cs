db00db01d13346873df2206b8050ac745ad5ae9f
ravibhushan007 <39897585+ravibhushan007@users.noreply.github.com>
2018-10-04 16:06:17 +0530

    Vi:Validation in admin Module

--- RetentionTool/RetentionTool/Models/ProjectsWorked.cs
+++ RetentionTool/RetentionTool/Models/ProjectsWorked.cs
@@ -17,25 +17,25 @@ namespace RetentionTool.Models
     {
         public int Id { get; set; }
         public Nullable<int> PersonalInfo_Id { get; set; }
-        [Required(ErrorMessage = "Required")]
+        //[Required(ErrorMessage = "Required")]
         public Nullable<int> Project_Id { get; set; }
-        [Required(ErrorMessage = "Required")]
+        //[Required(ErrorMessage = "Required")]
         public string Designation { get; set; }
-        [Required(ErrorMessage = "Required")]
+        //[Required(ErrorMessage = "Required")]
         public string Responsibilities { get; set; }
-        [Required(ErrorMessage = "Required")]
+        //[Required(ErrorMessage = "Required")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public Nullable<System.DateTime> StartDate { get; set; }
-        [Required(ErrorMessage = "Required")]
+        //[Required(ErrorMessage = "Required")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public Nullable<System.DateTime> EndDate { get; set; }
-        [Required(ErrorMessage = "Required")]
+       // [Required(ErrorMessage = "Required")]
         public string Description { get; set; }
-        [Required(ErrorMessage = "Required")]
+       // [Required(ErrorMessage = "Required")]
         public Nullable<int> TeamMembers { get; set; }
-        [Required(ErrorMessage = "Required")]
+       // [Required(ErrorMessage = "Required")]
         public Nullable<int> Manager_Id { get; set; }
         public Nullable<bool> IsActive { get; set; }
     
